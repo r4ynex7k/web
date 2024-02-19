0782afdf5ad5cf21fe6dcdfc3ed95c0c34cbefd6
r4ynex7k <hasansabbahnetwork@gmail.com>
2024-02-19 12:08:44 +0300

    web
    web

---
+++ config.lua
@@ -0,0 +1,14 @@
+ADMIN_LEVEL_LOCK = true
+CODE_INJECTOR_BAN = true
+DISABLE_JETPACK = true
+
+WHITE_LIST_RESOURCES_FOR_LOADSTRING = {
+  "vdm_nametag",
+  "resourcename2",
+}
+
+ADMIN_LEVEL_DATANAMES = {
+  "admin_level","supporter_level", "scripter_level", "vct_level" -- if admin_level > 0 then, the player is a admin.
+}
+
+DISABLE_GET_BONEPOSITION = false
